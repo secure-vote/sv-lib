bc7abe4902d46d16c8759345a51bd221f06ed582
Max Kaye <m@xk.io>
2018-07-20 16:21:59 +1000

    bad prettier, bad

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -10,44 +10,7 @@ test('hex to uint8', () => {
     expect(u.hexToUint8Array('0xff')).toEqual(new Uint8Array([0xff]))
     expect(u.hexToUint8Array('0x1337')).toEqual(new Uint8Array([0x13, 0x37]))
 
-    expect(
-        u.hexToUint8Array(
-            '0xd733f86fba82cc45ea7055d0bef76af6ddcd0e40996840362995f0da764a627b'
-        )
-    ).toEqual(
-        new Uint8Array([
-            215,
-            51,
-            248,
-            111,
-            186,
-            130,
-            204,
-            69,
-            234,
-            112,
-            85,
-            208,
-            190,
-            247,
-            106,
-            246,
-            221,
-            205,
-            14,
-            64,
-            153,
-            104,
-            64,
-            54,
-            41,
-            149,
-            240,
-            218,
-            118,
-            74,
-            98,
-            123
-        ])
-    )
+    // prettier-ignore
+    expect(u.hexToUint8Array('0xd733f86fba82cc45ea7055d0bef76af6ddcd0e40996840362995f0da764a627b'))
+        .toEqual(new Uint8Array([215,51,248,111,186,130,204,69,234,112,85,208,190,247,106,246,221,205,14,64,153,104,64,54,41,149,240,218,118,74,98,123]))
 })
