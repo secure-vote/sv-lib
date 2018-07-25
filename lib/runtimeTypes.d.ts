8487f1a08c5e7469f62373310403cf2dfbbacb4b
Max Kaye <m@xk.io>
2018-07-25 11:22:57 +1000

    Remove /dist and /lib

--- lib/runtimeTypes.d.ts
+++
@@ -1,4 +0,0 @@
-import * as t from 'io-ts';
-export declare const HexString: t.RefinementType<t.StringType, string, string, t.mixed>;
-export declare const Bytes32: t.RefinementType<t.RefinementType<t.StringType, string, string, t.mixed>, string, string, t.mixed>;
-export declare const Bytes64: t.RefinementType<t.RefinementType<t.StringType, string, string, t.mixed>, string, string, t.mixed>;
