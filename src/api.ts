43a6f0c7bfb6857d01b195f5ebd9e515bfa9c848
Max Kaye <m@xk.io>
2018-08-06 15:53:39 +1000

    prep v0.2.2 release

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
-import axios, { AxiosResponse, AxiosError } from 'axios'
+import { AxiosResponse, AxiosError, default as axios } from 'axios'
 
 import { ApiError } from './errors'
 import { EthNetConf, Ed25519DelegationResp } from './types'
 
-export const processApiError = <T>(err: AxiosError): T => {
-    throw err.response.status && err.response.status === 400 && (err.response.data as any).error ? new ApiError((err.response.data as any).error) : err
+export const processApiError = (err: AxiosError) => {
+    throw (err.response && err.response.status === 400) && err.response.data.error ? new ApiError(err.response.data.error) : err
 }
 
-export const extractData = <T>(req: AxiosResponse): T => req.data as T
+export const extractData = <T>(req: AxiosResponse<T>): T => req.data as T
 
 /**
  * Generate the submitEd25519Delegation API URL
